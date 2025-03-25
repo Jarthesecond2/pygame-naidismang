ee0a6a280ab3ceb1da7ad7f3f9c356894335b7e5
Janis <janis.kaldmaa@voco.ee>
2025-03-25 11:07:02 +0200

    game

--- game.py
+++ game.py
@@ -1,14 +1,18 @@
 import pygame
+from settings import Settings
 
 pygame.init()
-screen = pygame.display.set_mode((800, 600))
+gm_set = Settings()
+screen = pygame.display.set_mode((gm_set.screen_width, gm_set.screen_height))
+pygame.display.set_caption(gm_set.caption)
 running = True
+
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
             
-    screen.fill("#bd08b1")
+    screen.fill(gm_set.bg_color)
     pygame.display.flip()
 
 pygame.quit()
